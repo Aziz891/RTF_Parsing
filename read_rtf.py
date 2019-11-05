c6e170331680b33c78a92d4ed9cab065d36ae46e
Abdulaziz Alaboodi <Aziz.aboudi89@gmail.com>
2019-11-05 11:54:17 +0300

    first commit

--- read_rtf.py
+++ read_rtf.py
@@ -1,21 +1,47 @@
 import pandas as pd
 import re
-from tkinter import *
+import tkinter as tk
+from tkinter import filedialog
+import os
 rtf_list = []
 regex = re.compile(r'^\S* No packages have been outaged;')
-regex_end = re.compile(r'^.*Fault cleared at')
-
+regex_end = re.compile(r'^{\\f0\\fs16\\cf4  \\par')
+regex_fault_info = re.compile(r'{(\\cf4|\\cf1\\b|\\cf2\\b|\\cf3\\b) (.{5}) (.{40}) (.{10}) (.{50}) (.{9}) (.{26})\\par')
+regex_element_details = re.compile(r'{\\cf5\\b Element: (\d*) (\w*) "(\w*)".*; \((.*)\)\s*; Contact Logic Code: (\w*)\s*; Op. Time:\s{0,3}(\S*)\\par}')
+regex_circuit_details = re.compile(r'^{(\\cf4|\\cf2\\b|\\cf1\\b) (.{0,20}) (.{35}) (.{5}) (.{7}) (.{6}) (.{5}) (.{6}) (.{6}) (.*)\\par}')
+directory = 'C:/Users/aziza/Documents/Projects/cape_rtf/rtf_files'
 element_pd = pd.DataFrame()
 
-root = Tk()
 
-mybutton = Button(root, text = 'Exit')
-mybutton.pack()
-root.mainloop()
+# class Demo1:
+#     def __init__(self, master):
+#         self.master = master
+#         self.frame = tk.Frame(self.master, height=50)
+#         self.button1 = tk.Button(self.frame, text = 'New Window', width = 25 , command = self.new_window)
+#         self.button1.pack()
+#         self.frame.pack()
+#     def new_window(self):
+#         self.newWindow = tk.Toplevel(self.master)
+#         self.app = Demo2(self.newWindow)
 
+# class Demo2:
+#     def __init__(self, master):
+#         self.master = master
+#         self.frame = tk.Frame(self.master)
+#         self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+#         self.quitButton.pack()
+#         self.frame.pack()
+#     def close_windows(self):
+#         self.master.destroy()
 
+# def main(): 
+#     root = tk.Tk()
+#     app = Demo1(root)
+#     root.mainloop()
 
-def fault_parse(start):
+# main()
+
+def fault_parse(start, fault_list):
     temp = []
     for line in rtf_list[start:]:
         
@@ -34,7 +60,7 @@ def fault_to_panda(fault):
     for i in fault:
 
         if fault_info:
-            match_reg = re.findall(r'{(\\cf4|\\cf2\\b|\\cf1\\b) (.{5}) (.{40}) (.{10}) (.{50}) (.{9}) (.{26})\\par', i)
+            match_reg = regex_fault_info.findall(i)
             for index, j in enumerate(match_reg[0]):
                 
                 # if index in [0, 3]:
@@ -52,13 +78,18 @@ def fault_to_panda(fault):
 
 
 
-            print
+            
             fault_info = False
             
 
 
-        if re.match(r'^----- ', i):
+        if re.match(r'^----- ---------------------------------------- ', i):
              fault_info = True
+        if re.match(r'^{\\f0\\fs16\\cf4\\qc\\ul', i) :
+            #  if element_list.__len__() == 0:
+            #      pass
+            #  continue
+            pass
 
 
         if fault_details:
@@ -67,21 +98,21 @@ def fault_to_panda(fault):
               
 
               if re.match(r'^{\\cf5\\b Element: ', i):
-                  element_details = re.findall(r'{\\cf5\\b Element: (\d*) (\w*) "(\w*)" "(\w*)"; \((.*)\)\s*; Contact Logic Code: (\w*)\s*; Op. Time:  (.*)\\par}', i)
+                  element_details = regex_element_details.findall( i)
                   element_list.append([*temp[:-1], *circuit_details[0][1:], *element_details[0]])
-                  print
+                
 
-              elif re.match(r'^{\\cf6\\b   Sup. : ', i):
-                  print
-              elif re.match(r'{\\cf4  \\par}', i):
-                  print
+            #   elif re.match(r'^{\\cf6\\b   Sup. : ', i):
+            #       print
+            #   elif re.match(r'{\\cf4  \\par}', i):
+            #       print
 
               elif re.match(r'{\\cf4 ----------------', i):
                   fault_details = False
 
               if re.match(r'^{(\\cf4|\\cf2\\b|\\cf1\\b)', i):
-                  circuit_details = re.findall(r'^{(\\cf4|\\cf2\\b|\\cf1\\b) (.{0,20}) (.{35}) (.{5}) (.{7}) (.{6}) (.{5}) (.{6}) (.{6}) (.*)\\par}', i)
-                  print            
+                  circuit_details = regex_circuit_details.findall( i)
+                       
 
 
 
@@ -96,30 +127,61 @@ def fault_to_panda(fault):
 
 
 
-with open('NG_Coord_Review_380KV_(MHYL_WEST_49200)_(334_334)_(334_334)_CKT1.rtf', 'r') as f:
-    for i in f:
-        if i != '\n':
-            rtf_list.append(i)
-    
-fault_list = []
-iter_rtf =  iter(rtf_list)
 
-for index, i in enumerate(rtf_list):
-     if regex.match(i):
+def parse_file(rtf):
 
-         fault_parse(index)
+        
+        element_pd_list = []
+        fault_list = []
+        iter_rtf =  iter(rtf)
+        count = 0
+        result_faults = []
+        for index, i in enumerate(rtf):
+            if regex.match(i):
 
-for i in fault_list:
-    result_faults = fault_to_panda(i)
-    element_pd = element_pd.append(result_faults, )
+             
 
-element_pd.columns =['column' + str(i) for i in range(22)]
-element_pd.set_index(['column4',  'column6', 'column7', 'column17', 'column2'  ], inplace=True)
-element_pd = element_pd.sort_index()
-element_pd = element_pd[element_pd['column14'] != 'NORMAL OPERATION']
-element_pd.to_excel('output.xlsx')
+                fault_parse(index, fault_list)
 
-print()
+        for i in fault_list:
+            result_faults = result_faults + fault_to_panda(i)
+            print
+            # for i in result_faults:
+            #     if i.__len__() != 21:
+            #         print
+      
+                    
+            
+        
+        print('finished')
+        return result_faults
+
+
+def parse_coordination_files(dirct_path)
+    for filename in os.listdir(directory):
+        if filename.endswith(".rtf") and not filename.endswith('summary.rtf') : 
+            with open( 'rtf_files/' + filename, 'r') as f:
+                rtf_list = []
+                print(filename)
+                for i in f:
+                        if i != '\n':
+                            rtf_list.append(i)
+            element_pd = element_pd.append(parse_file(rtf_list)  )
+    print('done')
+
+
+    element_pd.columns =['Outage Number', 'Contingency', 'Fault Type', 'Fault Location' , 'Line Under Study', 'LZOP Fault Clearing Time', 'Substation', 'LZOP Name', 'LZOP Type', 'Primary/Backup', 'LZOP Time'
+    , 'Breaker Time', 'Total Time', 'CTI', 'Operation', 'Relay Tag', 'Element Type' , 'Element', 'Relay Type', 'Contact Logic Code', 'Element Operation Time']
+    element_pd = element_pd[element_pd['Operation'] != 'NORMAL OPERATION']
+    test = element_pd['LZOP Time']
+    element_pd = element_pd[(element_pd['Element Operation Time'].astype(float) >= element_pd['LZOP Time'].astype(float).multiply(0.9) ) & ( element_pd['Element Operation Time'].astype(float) <= element_pd['LZOP Time'].astype(float).multiply(1.1)) ]
+    element_pd.set_index(['Line Under Study',  'Substation', 'LZOP Name',  'Contingency', 'Element', 'Fault Type' ], inplace=True)
+    element_pd = element_pd.sort_index()
+    print('generating excel ')
+
+    element_pd.to_excel('output.xlsx', engine='xlsxwriter')
+
+    print()
 
 
 
